54dfa7703a7a4f57fefc98f09c9a5b0a66f6b64a
pietro <quadrinipietro@gmail.com>
2021-06-22 21:59:22 +0200

    primo Commit

--- siw-spring-museo/src/main/java/it/uniroma3/siw/spring/controller/MuseoController.java
+++ siw-spring-museo/src/main/java/it/uniroma3/siw/spring/controller/MuseoController.java
@@ -1,16 +1,11 @@
 package it.uniroma3.siw.spring.controller;
 
 
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import it.uniroma3.siw.spring.service.OperaService;
 
 
 
@@ -18,8 +13,6 @@ import it.uniroma3.siw.spring.service.OperaService;
 @Controller
 public class MuseoController {
 
-  @Autowired
-  private OperaService operaService;
 	
   @RequestMapping(value={"/home","/"}, method=RequestMethod.GET)
   public String goHome(Model model) {
